b499e6c15bde74a1d86e61a1c2940cf4329ad93a
Vinod Gupta <vinod_gupta@live.com>
2015-01-30 18:59:07 -0800

    Find anagrams in english dictionary

--- Anagrams/main.cpp
+++ Anagrams/main.cpp
@@ -12,89 +12,112 @@
 #include <list>
 #include <map>
 #include <unordered_map>
+#include <fstream>
 
-std::vector<int> prime_vct;
-
-//Copied form gist https://gist.github.com/vinodg/a9bf40195c7d3a211e0d
-void primeGenerator(std::vector<int> &prime_vct, unsigned int maxPrime)
+class Anagrams
 {
-    prime_vct.push_back(1);
-    prime_vct.push_back(2);
-    
-    for (unsigned int i = 3; i < maxPrime; i += 2)
+public:
+    typedef std::vector<std::string> StringVector;
+    typedef std::unordered_map<int, StringVector> HashAnagram;
+
+    Anagrams()
     {
-        int found = 1;
-        double sqrt_i = sqrt(i);
-        std::vector<int>::iterator itr = prime_vct.begin();
-        while (itr != prime_vct.end())
+        primeGenerator(prime_vct, 200);
+    }
+    virtual ~Anagrams()
+    {
+        
+    }
+    
+    void ReadWords()
+    {
+        std::ifstream ifs("/usr/share/dict/words", std::ifstream::in);
+        char word[256];
+        
+        while(ifs.getline(word,256))
         {
-            if (*itr >= 3)
+            hash[GetHashKey(word)].push_back(word);
+        }
+    }
+    
+    void PrintAnagrams()
+    {
+        for (HashAnagram::iterator itr = hash.begin(); itr != hash.end(); ++itr)
+        {
+            if(itr->second.size() <2)
+                continue;
+            
+            std::cout<<"{";
+            for (StringVector::iterator itr2 = itr->second.begin();
+                 itr2 != itr->second.end(); ++itr2)
             {
-                if (*itr> sqrt_i)
-                    break;
-                
-                if (i%*itr == 0)
-                {
-                    found = 0;
-                    break;
-                }
+                std::cout << itr2->c_str() << ", ";
             }
-            ++itr;
-        }
-        if (found)
-        {
-            prime_vct.push_back(i);
-            if (prime_vct.size() > maxPrime - 1)
-                break;
+            std::cout <<"}"<< std::endl;
         }
     }
     
-}
-
-int GetHashKey(const char* word)
-{
-    int i = 0;
-    int key = 1;
-    while (word[i] != '\0')
+private:
+    void primeGenerator(std::vector<int> &prime_vct, unsigned int maxPrime)
     {
-        key *= prime_vct[word[i] - 'a'];
-        ++i;
+        prime_vct.push_back(2);
+        
+        for (unsigned int i = 3; i < maxPrime; i += 2)
+        {
+            int found = 1;
+            double sqrt_i = sqrt(i);
+            std::vector<int>::iterator itr = prime_vct.begin();
+            while (itr != prime_vct.end())
+            {
+                if (*itr >= 3)
+                {
+                    if (*itr> sqrt_i)
+                        break;
+                    
+                    if (i%*itr == 0)
+                    {
+                        found = 0;
+                        break;
+                    }
+                }
+                ++itr;
+            }
+            if (found)
+            {
+                prime_vct.push_back(i);
+                if (prime_vct.size() > maxPrime - 1)
+                    break;
+            }
+        }
     }
     
-    return key;
-}
+    int GetHashKey(const char* word)
+    {
+        int i = 0;
+        int key = 1;
+        while (word[i] != '\0')
+        {
+            char c = word[i];
+            
+            if(c >= 'A' && c <= 'Z')
+                c='a' + (c-'A');
+            
+            key *= prime_vct[c - 'a'];
+            ++i;
+        }
+        
+        return key;
+    }
+
+    std::vector<int> prime_vct;
+    HashAnagram hash;
+};
 
 int main(int argc, char* argv[])
 {
-    typedef std::vector<std::string> StringVector;
-    typedef std::unordered_map<int, StringVector> HashAnagram;
-    HashAnagram hash;
+    Anagrams anagrams;
     
-    primeGenerator(prime_vct, 100);
-    StringVector test_set;
-    test_set.push_back("act");
-    test_set.push_back("vinod");
-    test_set.push_back("rat");
-    test_set.push_back("atr");
-    test_set.push_back("tar");
-    test_set.push_back("cat");
-    test_set.push_back("inodv");
-    
-    
-    for (StringVector::iterator itr = test_set.begin();
-         itr != test_set.end(); ++itr)
-    {
-        hash[GetHashKey(itr->c_str())].push_back(*itr);
-    }
-    
-    for (HashAnagram::iterator itr = hash.begin(); itr != hash.end(); ++itr)
-    {
-        for (StringVector::iterator itr2 = itr->second.begin();
-             itr2 != itr->second.end(); ++itr2)
-        {
-            std::cout << itr2->c_str() << "\t";
-        }
-        std::cout << std::endl;
-    }
+    anagrams.ReadWords();
+    anagrams.PrintAnagrams();
     return 0;
 }
